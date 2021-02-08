5a8d45bd0b5c473f2251afb9b15239a0177d8d91
dong <chiadong07@gmail.com>
2021-02-08 10:56:04 +0800

    Splash screen w anim

--- main/java/com/sp/notesapp/SplashScreen.java
+++ main/java/com/sp/notesapp/SplashScreen.java
@@ -7,12 +7,21 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import org.w3c.dom.Text;
+
 public class SplashScreen extends AppCompatActivity {
     MediaPlayer mp;
+    Animation topAnim, btmAnim;
+    ImageView img;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +29,20 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
 
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance(); //auth object;
-
         //current user object = it is the currently logged in user. If not logged in, this is null.
         final FirebaseUser currentUser= firebaseAuth.getCurrentUser();
 
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        btmAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        img= findViewById(R.id.splashScreenImage);
+        text= findViewById(R.id.splashScreenText);
+
+        text.setAnimation(topAnim);
+        img.setAnimation(btmAnim);
 
         mp = MediaPlayer.create(getBaseContext(), R.raw.music);
         mp.start(); //Starts your sound
-
 
 
         Handler handler = new Handler();
@@ -52,7 +67,7 @@ public class SplashScreen extends AppCompatActivity {
                 }
 
             }
-        },2000);
+        },3000);
     }
 
     @Override
